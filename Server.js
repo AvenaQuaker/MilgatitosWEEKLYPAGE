c14431e91da80007f92e6d43428e36247940f8ce
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-10-11 09:04:09 -0500

    Prueba BETA FINAL

---
+++ Server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT ?? 1234;
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(process.cwd(), "src", "index.html"));
+// });
+
+app.post("/Guardar", (req, res) => {
+    const eventos = req.body;
+    const file = path.join(process.cwd(), "Datos.json");
+    const tempFile = path.join(process.cwd(), "Datos.tmp.json");
+
+    fs.writeFile(tempFile, JSON.stringify(eventos), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error al guardar los eventos");
+        } else {
+            fs.rename(tempFile, file, (renameErr) => {
+                if (renameErr) {
+                    console.error(renameErr);
+                    res.status(500).send("Error al renombrar el archivo");
+                } else {
+                    res.send("Eventos guardados correctamente");
+                }
+            });
+        }
+    });
+});
+
+app.get("/Cargar", (req, res) => {
+    const file = path.join(process.cwd(), "Datos.json");
+
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error al cargar los eventos");
+        } else {
+            const eventos = JSON.parse(data);
+            res.send(eventos);
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ let eventador = document.getElementById("eventador");
 let Cerrar = document.querySelectorAll(".btnCerrar");
 let Cancelar = document.getElementById("Cancelar");
 let celdaActual;
-let Duracion;
+let Duracion = -1;
+let EventosProgramados = [];
+let EventosHoy = [];
+
+let Dias = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo",
+];
 
 celdas.forEach((celda) => {
     celda.addEventListener("click", () => {
@@ -30,49 +42,31 @@ document.addEventListener("click", (e) => {
     }
 });
 
-eventador.querySelector("button").addEventListener("click", () => {
+eventador.querySelector("button").addEventListener("click", (e) => {
     eventador.classList.add("opacity-0");
     eventador.classList.add("pointer-events-none");
 
-    let Datos = new FormData(eventador);
-    let objeto = {};
-    Datos.forEach((entries, key) => {
-        objeto[key] = entries;
-    });
+    let tipo = document.getElementById("tipoEvento").value;
+    let duracion = document.getElementById("duraEvento").value;
+    let mensaje = document.getElementById("msg").value;
+    let variado = document.getElementById("eventoVariado").value;
 
-    ProgramarEvento(celdaActual, objeto);
-});
-
-Cancelar.querySelector("button").addEventListener("click", () => {
-    CerrarElemento(Cancelar);
-
-    contador = parseInt(celdaActual.querySelector("p").textContent);
-    while (contador > 1) {
-        let caja = document.createElement("div");
-        caja.classList.add("celda");
-        celdaActual.parentNode.append(caja);
-        contador -= 1;
-    }
-
-    celdaActual.classList.remove("SELECTED");
-    celdaActual.style = "";
-    celdaActual.classList = "celda";
-    celdaActual.innerHTML = "";
-});
-
-function ActivarForma(celda, Seleccionado) {
-    if (!Seleccionado) {
-        eventador.classList.remove("opacity-0");
-        eventador.classList.remove("pointer-events-none");
+    let objeto;
+    if (!variado == "") {
+        objeto = {
+            Tipo: variado,
+            Duracion: duracion,
+            Mensaje: mensaje,
+        };
     } else {
-        Cancelar.classList.remove("opacity-0");
-        Cancelar.classList.remove("pointer-events-none");
+        objeto = {
+            Tipo: tipo,
+            Duracion: duracion,
+            Mensaje: mensaje,
+        };
     }
 
-    celdaActual = celda;
-}
-
-function ProgramarEvento(celda, objeto) {
+    let celda = celdaActual;
     let CeldasSeleccionadas = celda.parentNode.children;
     let indiceCelda = Array.from(CeldasSeleccionadas).indexOf(celda);
     let bandera = true;
@@ -92,41 +86,85 @@ function ProgramarEvento(celda, objeto) {
         }
     }
 
+    let indiceDia = Array.from(celda.parentNode.parentNode.children).indexOf(
+        celda.parentNode
+    );
+
+    let DiaSeleccionado = Dias[indiceDia];
+
     if (bandera) {
-        let Tipo = objeto.Tipo;
-        let Recursos = obtenerRecrusos(Tipo);
-        // Expandir la primera celda para cubrir el rango
-        celda.classList.add("bg-cover", "SELECTED");
-        celda.style.backgroundImage = `url('${Recursos.Fondo}')`;
-        celda.style.gridRow = `span ${duracionFinal}`;
-
-        let imagenSuper = document.createElement("img");
-        let Asunto = document.createElement("h1");
-        let Mensaje = document.createElement("h2");
-        let horas = document.createElement("p");
-        imagenSuper.src = Recursos.Logo;
-
-        Asunto.textContent = Tipo;
-        Mensaje.textContent = objeto.Mensaje;
-        horas.textContent = `${duracionFinal}`;
-
-        imagenSuper.classList.add("imgSuper");
-        horas.classList.add("hidden");
-        Asunto.classList.add("Asunto");
-        Mensaje.classList.add("Mensaje");
-
-        celda.append(horas);
-        celda.append(imagenSuper);
-        celda.append(Asunto);
-        celda.append(Mensaje);
-
-        // Eliminar las celdas que se "cubrieron" con la expansión
-        for (let i = 1; i < duracionFinal; i++) {
-            celda.parentNode.removeChild(CeldasSeleccionadas[indiceCelda + 1]);
-        }
+        AnadirEvento(
+            celda,
+            objeto,
+            duracionFinal,
+            DiaSeleccionado,
+            indiceCelda,
+            CeldasSeleccionadas
+        );
     } else {
         console.log("Algunas de las celdas están ocupadas.");
     }
+});
+
+Cancelar.querySelector("button").addEventListener("click", () => {
+    CerrarElemento(Cancelar);
+    let indiceHora = Array.from(celdaActual.parentNode.children).indexOf(
+        celdaActual
+    );
+    let indiceDia = Array.from(
+        celdaActual.parentNode.parentNode.children
+    ).indexOf(celdaActual.parentNode);
+    let dia = Dias[indiceDia];
+
+    // Filtramos los eventos para eliminar el evento correspondiente a la hora y día
+    EventosProgramados = EventosProgramados.filter((evento) => {
+        return !(evento.Dia === dia);
+    });
+
+    let CeldasSemana = Array.from(celdaActual.parentNode.children);
+    CeldasSemana.forEach((celdaActual) => {
+        if (celdaActual.classList.contains("SELECTED")) {
+            let contador = parseInt(celdaActual.querySelector("p").textContent);
+
+            celdaActual.classList.remove("SELECTED");
+            celdaActual.style = "";
+            celdaActual.classList = "celda";
+            celdaActual.innerHTML = "";
+
+            let celdaSiguiente = celdaActual.nextSibling.nextElementSibling;
+
+            while (contador > 1) {
+                let caja = document.createElement("div");
+                caja.classList.add("celda");
+                caja.addEventListener("click", () => {
+                    let Seleccionado = false;
+                    if (caja.classList.contains("SELECTED"))
+                        Seleccionado = true;
+
+                    ActivarForma(caja, Seleccionado);
+                });
+
+                if (celdaSiguiente) {
+                    celdaActual.parentNode.insertBefore(caja, celdaSiguiente);
+                } else {
+                    celdaActual.parentNode.append(caja);
+                }
+                contador -= 1;
+            }
+        }
+    });
+});
+
+function ActivarForma(celda, Seleccionado) {
+    if (!Seleccionado) {
+        eventador.classList.remove("opacity-0");
+        eventador.classList.remove("pointer-events-none");
+    } else {
+        Cancelar.classList.remove("opacity-0");
+        Cancelar.classList.remove("pointer-events-none");
+    }
+
+    celdaActual = celda;
 }
 
 function obtenerRecrusos(Tipo) {
@@ -162,33 +200,34 @@ function obtenerRecrusos(Tipo) {
 
     let Recursos = {
         Fondo: "",
-        Logo: "",
+        GIF: "",
     };
 
     switch (Tipo) {
         case "HOTS":
             Recursos.Fondo = HotsFondos[ObtenerIndice(HotsFondos)];
-            Recursos.Logo = "./Resources/HotsLogo.png";
+            Recursos.GIF = "./Resources/Hots.gif";
             break;
         case "IRL":
             Recursos.Fondo = IRLFondos[ObtenerIndice(IRLFondos)];
-            Recursos.Logo = "./Resources/IRLLogo.png";
+            Recursos.GIF = "./Resources/IRL.gif";
             break;
         case "Anime":
             Recursos.Fondo = AnimeFondos[ObtenerIndice(AnimeFondos)];
-            Recursos.Logo = "./Resources/AnimeLogo.png";
+            Recursos.GIF = "./Resources/Anime.gif";
             break;
         case "Hearthstone":
             Recursos.Fondo =
                 HearthstoneFondos[ObtenerIndice(HearthstoneFondos)];
-            Recursos.Logo = "./Resources/HearthstoneLogo.png";
+            Recursos.GIF = "./Resources/HS.gif";
             break;
         case "Minecraft":
             Recursos.Fondo = MinecraftFondos[ObtenerIndice(MinecraftFondos)];
-            Recursos.Logo = "./Resources/MinecraftLogo.png";
+            Recursos.GIF = "./Resources/Minecraft.gif";
             break;
         default:
-            console.log("Tipo de evento no válido.");
+            Recursos.Fondo = "./Resources/VariadoBG.PNG"; // Aquí se debería seleccionar un fondo aleatorio
+            Recursos.GIF = "./Resources/Variado.gif"; // Aquí se debería seleccionar un GIF aleatorio
             break;
     }
 
@@ -204,3 +243,162 @@ function CerrarElemento(elemento) {
     elemento.classList.add("opacity-0");
     elemento.classList.add("pointer-events-none");
 }
+
+function AnadirEvento(
+    celda,
+    objeto,
+    duracionFinal,
+    DiaSeleccionado,
+    indiceCelda,
+    CeldasSeleccionadas
+) {
+    let Tipo = objeto.Tipo;
+    let Recursos = obtenerRecrusos(Tipo);
+
+    celda.classList.add("bg-cover", "SELECTED");
+    celda.style.backgroundImage = `url('${Recursos.Fondo}')`;
+    celda.style.gridRow = `span ${duracionFinal}`;
+
+    let Asunto = document.createElement("h1");
+    let Mensaje = document.createElement("h2");
+    let horas = document.createElement("p");
+
+    Asunto.textContent = Tipo;
+    Mensaje.textContent = objeto.Mensaje;
+    horas.textContent = `${duracionFinal}`;
+
+    horas.classList.add("hidden");
+    Asunto.classList.add("Asunto");
+    Mensaje.classList.add("Mensaje");
+
+    celda.append(horas);
+    celda.append(Asunto);
+    celda.append(Mensaje);
+
+    EventosProgramados.push({
+        Dia: DiaSeleccionado,
+        indice: indiceCelda,
+        Tipo: Tipo,
+        Mensaje: objeto.Mensaje,
+        Duracion: duracionFinal,
+    });
+
+    // Eliminar las celdas que se "cubrieron" con la expansión
+    for (let i = 1; i < duracionFinal; i++) {
+        celda.parentNode.removeChild(CeldasSeleccionadas[indiceCelda + 1]);
+    }
+}
+
+function EventosDeHoy(eventos) {
+    let Eventos = eventos;
+
+    let Tabla = document.querySelector("#tablaHoy");
+
+    Eventos.forEach((evento, index) => {
+        let Elemento = document.createElement("tr");
+        Elemento.classList.add("hoy");
+        Elemento.innerHTML = `
+            <td>${index + 1}. ${evento.Tipo}</td>
+            <td>${evento.Mensaje}</td>
+        `;
+
+        const objeto = obtenerRecrusos(evento.Tipo);
+        Elemento.style.setProperty("--img", `url(${objeto.GIF})`);
+        Tabla.appendChild(Elemento);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hoy = new Date();
+    const diasSemana = [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miercoles",
+        "Jueves",
+        "Viernes",
+        "Sabado",
+    ];
+    const Dia = diasSemana[hoy.getDay()];
+
+    $.ajax({
+        url: "http://localhost:1234/Cargar",
+        method: "GET",
+        success: function (response) {
+            console.log("Recursos cargados correctamente:", response);
+            let horario = celdas[0].parentNode.parentNode;
+
+            // Cargar los recursos en las celdas
+            response.forEach((recurso) => {
+                let dia = Dias.indexOf(recurso.Dia);
+                let celda = horario.children[dia].children[recurso.indice];
+
+                let CeldasSeleccionadas = celda.parentNode.children;
+                let indiceCelda =
+                    Array.from(CeldasSeleccionadas).indexOf(celda);
+                let bandera = true;
+                Duracion = parseInt(recurso.Duracion);
+
+                // Calcular cuántas celdas hay disponibles
+                let celdasDisponibles =
+                    CeldasSeleccionadas.length - indiceCelda;
+                let duracionFinal = Math.min(Duracion, celdasDisponibles);
+
+                // Revisar si las celdas en el rango están ocupadas
+                for (let i = 0; i < duracionFinal; i++) {
+                    if (
+                        CeldasSeleccionadas[indiceCelda + i].classList.contains(
+                            "SELECTED"
+                        )
+                    ) {
+                        bandera = false; // Si alguna está ocupada, no se puede seleccionar
+                        break;
+                    }
+                }
+
+                let indiceDia = Array.from(
+                    celda.parentNode.parentNode.children
+                ).indexOf(celda.parentNode);
+
+                let DiaSeleccionado = Dias[indiceDia];
+
+                if (bandera) {
+                    AnadirEvento(
+                        celda,
+                        recurso,
+                        duracionFinal,
+                        DiaSeleccionado,
+                        indiceCelda,
+                        CeldasSeleccionadas
+                    );
+                }
+
+                if (recurso.Dia == Dia) {
+                    EventosHoy.push(recurso);
+                }
+            });
+
+            EventosDeHoy(EventosHoy);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error al cargar recursos:", error);
+        },
+    });
+});
+
+document.getElementById("btnGuardar").addEventListener("click", () => {
+    $.ajax({
+        url: "http://localhost:1234/Guardar",
+        method: "POST",
+        data: JSON.stringify(EventosProgramados),
+        contentType: "application/json",
+        success: function (response) {
+            console.log("Respuesta del servidor:", response); // Ver respuesta del servidor
+            alert("Eventos guardados correctamente.");
+        },
+        error: function (xhr, status, error) {
+            console.error("Error al guardar los eventos:", error); // Manejo de errores
+            alert("Hubo un error al guardar los eventos.");
+        },
+    });
+});
