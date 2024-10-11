1b98ba0e36347b756547e44d4320465dccca653c
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-10-11 12:36:32 -0500

    Localhost

--- Server.js
+++ Server.js
@@ -14,6 +14,7 @@ app.use(
                 "http://localhost:1234",
                 "https://milgatitosweeklypage.onrender.com",
                 "https://avenaquaker.github.io",
+                "http://127.0.0.1:5500",
             ];
 
             if (ACCEPTED_ORIGINS.includes(origin)) {
